9a4c9360599ecc276490669d12025a1b9f5d4e81
MichalObi <ndwz123@gmail.com>
2023-06-05 16:31:44 +0200

    Add module support for node files / prepare class based on schema

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
-const { default: weaviate } = require('weaviate-ts-client');
+import weaviate from 'weaviate-ts-client';
 
 const client = weaviate.client({
     scheme: 'http',
     host: 'localhost:8080',
 });
 
-client.misc
-    .liveChecker()
-    .do()
-    .then(res => {
-        console.log('res', res);
-    })
-    .catch(err => {
-        console.error('err', err);
-    });
+const liveChecker = await client.misc.liveChecker().do();
 
-client
-    .schema
-    .getter()
-    .do()
-    .then(schemaRes => {
-        console.log('schemaRes', schemaRes);
-    })
-    .catch(err => {
-        console.error('err', err);
-    });
+console.log('liveChecker', liveChecker);
+
+const schemaConfig = {
+    'class': 'Meme',
+    'vectorizer': 'img2vec-neural',
+    'vectorIndexType': 'hnsw',
+    'moduleConfig': {
+        'img2vec-neural': {
+            'imageFields': [
+                'image'
+            ]
+        }
+    },
+    'properties': [
+        {
+            'name': 'image',
+            'dataType': ['blob']
+        },
+        {
+            'name': 'text',
+            'dataType': ['string']
+        }
+    ]
+}
+
+// create 'MEME' schema
+// client.schema
+//     .classCreator()
+//     .withClass(schemaConfig)
+//     .do()
+//     .then(schemaRes => {
+//         console.log('schemaRes', schemaRes);
+//     })
+//     .catch(err => {
+//         console.error('err', err);
+//     });
+
+const schemaRes = await client.schema.getter().do();
+
+console.log('schemaRes', schemaRes);
