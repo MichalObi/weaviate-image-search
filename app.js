8b3077c140f637120023de66dac315034d2824a3
MichalObi <ndwz123@gmail.com>
2023-06-06 20:41:56 +0200

    Prepare result img

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 import weaviate from 'weaviate-ts-client';
+import { readFileSync, writeFileSync } from 'fs'
 
+// connect to db client
 const client = weaviate.client({
     scheme: 'http',
     host: 'localhost:8080',
 });
 
+// test check if db is online
 const liveChecker = await client.misc.liveChecker().do();
 
 console.log('liveChecker', liveChecker);
 
+// create schema for db
 const schemaConfig = {
     'class': 'Meme',
     'vectorizer': 'img2vec-neural',
@@ -47,3 +51,31 @@ const schemaConfig = {
 const schemaRes = await client.schema.getter().do();
 
 console.log('schemaRes', schemaRes);
+
+// store img to db
+
+// const img = readFileSync('./img/guy.jpg');
+// const b64 = Buffer.from(img).toString('base64');
+
+// await client.data.creator()
+//     .withClassName('Meme')
+//     .withProperties({
+//         image: b64,
+//         text: 'guy meme'
+//     })
+//     .do();
+
+const testImgName = 'aMEPeB1_460s.jpg';
+const test = Buffer.from(readFileSync(`./img/${testImgName}`)).toString('base64');
+
+const resImage = await client.graphql.get()
+    .withClassName('Meme')
+    .withFields(['image'])
+    .withNearImage({ image: test })
+    .withLimit(1)
+    .do();
+
+// Write result to filesystem
+const result = resImage.data.Get.Meme[0].image;
+
+writeFileSync('./result.jpg', result, 'base64');
