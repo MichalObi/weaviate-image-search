a1d6c6cc3b4952418557b500a6137226dd9871b0
MichalObi <ndwz123@gmail.com>
2023-06-09 17:30:03 +0200

    Script clean-up

--- script.js
+++ script.js
@@ -7,68 +7,83 @@ const client = weaviate.client({
     host: 'localhost:8080',
 });
 
-// check if db is online
-const liveChecker = await client.misc.liveChecker().do();
-console.log('liveChecker', liveChecker);
-
-// create schema for db
-const schemaConfig = {
-    'class': 'Meme',
-    'vectorizer': 'img2vec-neural',
-    'vectorIndexType': 'hnsw',
-    'moduleConfig': {
-        'img2vec-neural': {
-            'imageFields': [
-                'image'
-            ]
-        }
-    },
-    'properties': [
-        {
-            'name': 'image',
-            'dataType': ['blob']
-        },
-        {
-            'name': 'text',
-            'dataType': ['string']
-        }
-    ]
-}
-
 // create 'MEME' schema - run once after db init
-client.schema
-    .classCreator()
-    .withClass(schemaConfig)
-    .do()
-    .then(schemaRes => console.log('schemaRes', schemaRes))
-    .catch(err => console.error('err', err));
+const createDBSchema = async () => {
+    const schemaConfig = {
+        'class': 'Meme',
+        'vectorizer': 'img2vec-neural',
+        'vectorIndexType': 'hnsw',
+        'moduleConfig': {
+            'img2vec-neural': {
+                'imageFields': [
+                    'image'
+                ]
+            }
+        },
+        'properties': [
+            {
+                'name': 'image',
+                'dataType': ['blob']
+            },
+            {
+                'name': 'text',
+                'dataType': ['string']
+            }
+        ]
+    }
 
-const schemaRes = await client.schema.getter().do();
-console.log('schemaRes', schemaRes);
+    client.schema
+        .classCreator()
+        .withClass(schemaConfig)
+        .do()
+        .then(schemaRes => console.log('schemaRes', schemaRes))
+        .catch(err => console.error('err', err));
+
+    const schemaRes = await client.schema.getter().do();
+
+    console.log('Schema created!', schemaRes);
+};
 
 // store img to db - no loop, invoke for every meme in img folder
+const storeImgInDB = async () => {
+    const img = readFileSync('./img/guy.jpg'),
+        b64 = Buffer.from(img).toString('base64');
 
-// const img = readFileSync('./img/guy.jpg');
-// const b64 = Buffer.from(img).toString('base64');
+    await client.data.creator()
+        .withClassName('Meme')
+        .withProperties({
+            image: b64,
+            text: 'guy meme'
+        })
+        .do();
 
-// await client.data.creator()
-//     .withClassName('Meme')
-//     .withProperties({
-//         image: b64,
-//         text: 'guy meme'
-//     })
-//     .do();
+    console.log('img saved!');
+};
 
-const inputImgame = 'aMEPeB1_460s.jpg';
-const inputImgBase = Buffer.from(readFileSync(`./img/${inputImgame}`)).toString('base64');
+const findSimilarMeme = async () => {
+    const inputImgame = 'aMEPeB1_460s.jpg',
+        inputImgBase = Buffer.from(readFileSync(`./img/${inputImgame}`)).toString('base64'),
+        resImage = await client.graphql.get()
+            .withClassName('Meme')
+            .withFields(['image'])
+            .withNearImage({ image: inputImgBase })
+            .withLimit(1)
+            .do();
 
-const resImage = await client.graphql.get()
-    .withClassName('Meme')
-    .withFields(['image'])
-    .withNearImage({ image: inputImgBase })
-    .withLimit(1)
-    .do();
+    const result = resImage.data.Get.Meme[0].image;
 
-// write result to filesystem
-const result = resImage.data.Get.Meme[0].image;
-writeFileSync('./result.jpg', result, 'base64');
+    writeFileSync('./result.jpg', result, 'base64');
+
+    console.log('result img created!');
+}
+
+// check if db is online
+const liveChecker = await client.misc.liveChecker().do();
+
+if (liveChecker) {
+    //createDBSchema();
+    // storeImgInDB();
+    findSimilarMeme();
+} else {
+    console.error('db is not running');
+}
